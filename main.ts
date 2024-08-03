3d148c9c32b8a7025e789e88cf84381ad9604e45
mahdi44785 <mahdimug53@gmail.com>
2024-08-03 17:13:47 +0600

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,14 @@
-namespace airbit {
-    
-}
-	
+namespace Airbit {
+    //%blockid=Airbit 1.0
+    radio.setGroup(1)
+    basic.showLeds(`
+    . . . . .
+    . . . . .
+    . # # # .
+    . . . . .
+    . . . . .
+    `)
 
+    if (input.buttonIsPressed(Button.A)) {radio.sendNumber(0)}
+    if (input.buttonIsPressed(Button.A)) { radio.sendString("jj")}
+}
